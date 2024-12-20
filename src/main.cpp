8cc58542012a3147592fd5cd8de6e4dfe4119cab
Arcturus Prime <seanans135@gmail.com>
2024-12-20 15:52:38 -0600

    touch up

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,8 @@
 #include "Types.h"
 #include "VFTable.h"
 
-#include <cstdint>
-#include <cstdio>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sstream>
+#include <iomanip>
 #include <windows.h>
 
 CRITICAL_SECTION g_receiveLock;
@@ -15,17 +13,22 @@ void hook_24(RakNet::RakPeer* rakPeer, char _1)
 {
     EnterCriticalSection(&g_receiveLock);
 
+    std::stringstream buffer;
+
     for (uint32_t i = rakPeer->queue_2.head; i < rakPeer->queue_2.tail; i++)
     {
+        buffer << std::hex << std::setfill('0');
+
+        std::cout << "Incoming message:" << std::endl;
+
         auto packet = rakPeer->queue_2.array[i];
 
-        printf("Incoming message:\n");
-
         for (uint32_t j = 0; j < packet->size; j++) {
-            printf("0x%02x ", packet->data[j]);
+            buffer << std::setw(2) << (uint32_t) packet->data[j] << " "; 
         }
 
-        printf("\n\n");
+        std::cout << buffer.str() << std::endl << std::endl;
+        buffer.clear();
     }
 
     LeaveCriticalSection(&g_receiveLock);
@@ -41,31 +44,31 @@ void Attach()
 
     freopen_s((FILE**) stdout, "CONOUT$", "w", stdout);
 
-    printf("Searching for VFTable...\n");
+    std::cout << "Searching for VFTable..." << std::endl;
 
     auto region = StudioDumper::Region::from_module((uintptr_t) GetModuleHandle(NULL));
     auto vftable_optional = StudioDumper::VFTable::find(region, ".?AVRakPeer@RakNet@@");
 
     if (!vftable_optional.has_value())
     {
-        printf("Unable to locate VFTable address!\n");
+        std::cout << "Unable to locate VFTable address!" << std::endl;
         return;
     }
 
     g_vftable = vftable_optional.value();
 
-    printf("Found RakPeer VFTable at 0x%llx with length %llu!\n", g_vftable.get_address(), g_vftable.get_size());
+    std::cout << "Found RakPeer VFTable at 0x" << std::hex << g_vftable.get_address() << " with length " <<  g_vftable.get_size() << "!" << std::endl;
 
     g_vftable.hook(24, (uintptr_t) &hook_24);
 
-    printf("Hooked networking!");
+    std::cout << "Hooked networking!" << std::endl;
 }
 
 void Detach()
 {
     g_vftable.unhook(24);
 
-    printf("Unhooked networking!\n");
+    std::cout << "Unhooked networking!" << std::endl;
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
